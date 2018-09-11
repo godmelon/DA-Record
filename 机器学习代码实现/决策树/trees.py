f6a46fe9a678b969229a0722677aa5da53690574
Wang <1973919240@qq.com>
2018-09-11 16:36:41 +0800

    决策树构建

---
+++ 机器学习代码实现/决策树/trees.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from math import log
+import operator
+
+# 计算香浓熵，熵越高，混合的数据越多，信息量越大
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCount = {}
+    
+    for featVec in dataSet:
+        curretnLabel = featVec[-1]
+        if curretnLabel not in labelCount.keys():
+            labelCount[curretnLabel] = 0
+        labelCount[curretnLabel] += 1
+        
+    shannonEnt = 0.0
+    for key in labelCount:
+        prob = float(labelCount[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 0, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reduceFeatVec = featVec[:axis]
+            reduceFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reduceFeatVec)
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0; bestFeature = -1
+    
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount:
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items, key=operator.itemgetter(1),
+                              reverse=True)
+    
+    return sortedClassCount[0][0]
+        
+
+
+
+
